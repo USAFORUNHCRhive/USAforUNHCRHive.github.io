3d554e5505428bd27bcbb214a8160e9ddfa94c76
jonhu4u <jonathan@unrefugees.org>
2024-10-30 17:37:38 -0400

    testing linting checks

---
+++ test.py
@@ -0,0 +1,44 @@
+def testFunctionWithBadFormatting():
+    # This function has bad formatting
+    print("Hello, World!")
+    print("This is a test function with bad formatting.")
+    for i in range(10):
+        print(i)
+    print("Done.")
+
+testFunctionWithBadFormatting()
+
+import pandas as pd
+import numpy as np
+import os
+
+query = """
+SELECT
+    CASE
+        WHEN temp_soft.id IS NULL THEN o.accountid 
+        ELSE temp_soft.accountid
+    END AS account_id
+    , o.closedate
+    , SUM(o.amount) AS amount
+ FROM raw_salesforce.opportunity AS o
+ LIMIT 10
+"""
+
+# put the above in a function
+def fetchData(query):
+    import pandas_gbq
+    import pydata_google_auth
+    SCOPES = ['https://www.googleapis.com/auth/cloud-platform']
+    credentials = pydata_google_auth.get_user_credentials(SCOPES, auth_local_webserver=True)
+
+# Update the in-memory credentials cache (added in pandas-gbq 0.7.0).
+    pandas_gbq.context.credentials = credentials
+    pandas_gbq.context.project = "u4u-ds-prod-00"
+
+    df = pandas_gbq.read_gbq(query, dtypes={"closedate": "datetime64"})
+    return df
+
+test2 = fetchData(query)  # Call the function with the query
+
+for i in test2:
+    print(i)
