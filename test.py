6d35f174bba96a85fcab0f9dedf4ec9bcdd9b26b
jonhu4u <jonathan@unrefugees.org>
2024-10-30 18:15:13 -0400

    more linting 2

--- test.py
+++ test.py
@@ -6,6 +6,7 @@ def testFunctionWithBadFormatting():
         print(i)
     print("Done.")
 
+
 testFunctionWithBadFormatting()
 
 import pandas as pd
@@ -21,19 +22,24 @@ SELECT
  LIMIT 10
 """
 
+
 # put the above in a function
 def fetchData(query):
     import pandas_gbq
     import pydata_google_auth
-    SCOPES = ['https://www.googleapis.com/auth/cloud-platform']
-    credentials = pydata_google_auth.get_user_credentials(SCOPES, auth_local_webserver=True)
 
-# Update the in-memory credentials cache (added in pandas-gbq 0.7.0).
+    SCOPES = ["https://www.googleapis.com/auth/cloud-platform"]
+    credentials = pydata_google_auth.get_user_credentials(
+        SCOPES, auth_local_webserver=True
+    )
+
+    # Update the in-memory credentials cache (added in pandas-gbq 0.7.0).
     pandas_gbq.context.credentials = credentials
     pandas_gbq.context.project = "u4u-ds-prod-00"
 
     df = pandas_gbq.read_gbq(query)
     return df
+
 
 test2 = fetchData(query)  # Call the function with the query
 
